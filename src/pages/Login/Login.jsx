f42316d929afdebeb639fcaee9e17e27ecff6c12
minhchuong32 <chuongminh3225@gmail.com>
2025-06-29 22:34:14 +0700

    done login/register page ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-  return (
-    <div className='login'>
-        
-    </div>
-  )
-}
+  const [signState, setSignState] = useState("Đăng Nhập");
 
-export default Login
+  return (
+    <div className="login">
+      <img src={logo} className="login-logo" alt="Logo" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form action="">
+          {signState === "Đăng Kí" ? (
+            <input type="text" placeholder="Tên đăng nhập" />
+          ) : (
+            <> </>
+          )}
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Mật khẩu" />
+          <button type="submit">Đăng Nhập</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label>Nhớ mật khẩu</label>
+            </div>
+            <p>
+              Cần giúp đỡ ?  <a href="/help">Liên hệ hỗ trợ</a>
+            </p>
+          </div> 
+        </form>
+        <div className="form-switch">
+            {signState === "Đăng Nhập" ?<p>
+            Mới sử dụng Netflix? <span onClick={() => setSignState("Đăng Kí")}>Đăng ký ngay</span>
+          </p> :  <p>
+            Đã có tài khoản? <span onClick={() => setSignState("Đăng Nhập")}>Đăng nhập</span>
+          </p> }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
